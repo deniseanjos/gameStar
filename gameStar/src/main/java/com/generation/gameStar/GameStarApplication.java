70d62b8d5cc27da98d8a7792984b24cbe125b028
Denise Anjos <deniseb.anjos@outlook.com>
2021-02-25 12:19:31 -0300

    Envio inicial

---
+++ gameStar/src/main/java/com/generation/gameStar/GameStarApplication.java
@@ -0,0 +1,13 @@
+package com.generation.gameStar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStarApplication.class, args);
+	}
+
+}
---
+++ gameStar/src/main/java/com/generation/gameStar/model/Categoria.java
@@ -0,0 +1,5 @@
+package com.generation.gameStar.model;
+
+public class Categoria {
+
+}
